644be566dbd91e39efc3a8adeb1c396326967ec1
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-10 10:37:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
+
+checkMessageButton.addEventListener("click", () => {
+
+});
